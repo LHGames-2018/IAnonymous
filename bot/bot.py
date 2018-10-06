cca40c5992043d396232305d2d6db8b92ebb01ce
Badr El Berdai <seb_badr@hotmail.com>
2018-10-06 12:31:10 -0400

    collect adjacent

--- bot/bot.py
+++ bot/bot.py
@@ -20,6 +20,16 @@ class Bot:
         """
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
+        if gameMap.getTileAt(self, Point(self.PlayerInfo.Position.getX() + 1, self.Position.getY())) == 4:
+            create_collect_action(Point(1, 0))
+        elif gameMap.getTileAt(self, Point(self.PlayerInfo.Position.getX(), self.Position.getY() + 1)) == 4:
+            create_collect_action(Point(0, 1))
+        elif gameMap.getTileAt(self, Point(self.PlayerInfo.Position.getX() - 1, self.Position.getY())) == 4:
+            create_collect_action(Point(-1, 0))
+        elif gameMap.getTileAt(self, Point(self.PlayerInfo.Position.getX(), self.Position.getY() - 1)) == 4:
+            create_collect_action(Point(0, -1))
+        else:
+            create_move_action(Point(0, 1))
         return create_move_action(Point(1, 0))
 
     def after_turn(self):

2d5840202f6f6edcafaac6b61b1a96c21649d441
Badr El Berdai <seb_badr@hotmail.com>
2018-10-06 19:30:23 -0400

    pathfinder v3

--- bot/bot.py
+++ bot/bot.py
@@ -16,54 +16,53 @@ class Bot:
 
 
 
-    def execute_turn(self, gameMap, visiblePlayers):
-        """
-        This is where you decide what action to take.
-            :param gameMap: The gamemap.
-            :param visiblePlayers:  The list of visible players.
-        """
-        return create_move_action(Point(0,-1))
-    '''def plusProche(self, arrayDirection):
-        closestPoint = Point(-1, -1)
-        closestDistance = 1000
-        for i in arrayDirection:
-            if Point.Distance(self.PlayerInfo.Position, arrayDirection[i]) < closestDistance:
-                closestDistance = Point.Distance(self.PlayerInfo.Position, arrayDirection[i])
-                closestPoint = arrayDirection[i]
-        return closestPoint
+        def execute_turn(self, gameMap, visiblePlayers):
+            """
+            This is where you decide what action to take.
+                :param gameMap: The gamemap.
+                :param visiblePlayers:  The list of visible players.
+            """
+            def plusProche(self, arrayDirection):
+                closestPoint = Point(-1, -1)
+                closestDistance = 1000
+                for i in arrayDirection:
+                    if Point.Distance(self.PlayerInfo.Position, arrayDirection[i]) < closestDistance:
+                        closestDistance = Point.Distance(self.PlayerInfo.Position, arrayDirection[i])
+                        closestPoint = arrayDirection[i]
+                return closestPoint
 
-        # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        arrayDirection = []
-        for i in range(21):
-            for j in range(21):
-                if gameMap.getTileAt(
-                        Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j)) == TileContent.Resource:
-                    arrayDirection.append(Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j))
-        matrixPath = np.zeros([plusProche(self, arrayDirection).x, plusProche(self, arrayDirection).y])
-        grid = Grid(matrix=matrixPath)
-        start = grid.node(0, 0)
-        end = grid.node(StorageHelper.read("positionCible").getX(), StorageHelper.read("positionCible").getY())
+                # Write your bot here. Use functions from aiHelper to instantiate your actions.
+                arrayDirection = []
+                for i in range(21):
+                    for j in range(21):
+                        if gameMap.getTileAt(
+                                Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j)) == TileContent.Resource:
+                            arrayDirection.append(Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j))
+                matrixPath = np.zeros([plusProche(self, arrayDirection).x, plusProche(self, arrayDirection).y])
+                grid = Grid(matrix=matrixPath)
+                start = grid.node(0, 0)
+                end = grid.node(StorageHelper.read("positionCible").getX(), StorageHelper.read("positionCible").getY())
 
-        actualClosest = plusProche(self, arrayDirection)
-
-
-        xRange = abs(self.PlayerInfo.Position.x - actualClosest.x)
-        yRange = abs(self.PlayerInfo.Position.y - actualClosest.y)
-        matrixPath = np.zeros([xRange, yRange])
+                actualClosest = plusProche(self, arrayDirection)
 
 
-        for i in range(xRange):
-            for j in range(yRange):
-                if gameMap.getTileAt(self, Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j)) == 0 or gameMap.getTileAt(self, Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j)) == 4:
-                    matrixPath[i, j] = 1
-                else:
-                    matrixPath[i, j] = 0
+                xRange = abs(self.PlayerInfo.Position.x - actualClosest.x)
+                yRange = abs(self.PlayerInfo.Position.y - actualClosest.y)
+                matrixPath = np.zeros([xRange, yRange])
 
-        for y in range(len(self.nodes)):
-            for x in range(len(self.nodes[y])):
-                node = self.nodes[y][x]
-                if path and ((node.x, node.y) in path or node in path):
-                    return create_move_action(Point(node.x, node.y))'''
+
+                for i in range(xRange):
+                    for j in range(yRange):
+                        if gameMap.getTileAt(self, Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j)) == 0 or gameMap.getTileAt(self, Point(self.PlayerInfo.Position.x + i, self.PlayerInfo.Position.y + j)) == 4:
+                            matrixPath[i, j] = 1
+                        else:
+                            matrixPath[i, j] = 0
+
+                for y in range(len(self.nodes)):
+                    for x in range(len(self.nodes[y])):
+                        node = self.nodes[y][x]
+                        if path and ((node.x, node.y) in path or node in path):
+                            return create_move_action(Point(node.x, node.y))
 
     def after_turn(self):
         """
